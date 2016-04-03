5ef815701f100fd13d05bbfa40ae05d7918ee973
vain0x <vainzerox@gmail.com>
2016-04-03 20:25:50 +0900

    [cui] First version

---
+++ TriTrain.Cui/Broadcaster.fs
@@ -0,0 +1,99 @@
+﻿namespace TriTrain.Cui
+
+open System
+open TriTrain.Core
+
+module Broadcaster =
+  let stringizeSide =
+    function
+    | PlLft -> "左陣"
+    | PlRgt -> "右陣"
+
+  let stringizeVertex =
+    function
+    | Fwd -> "前"
+    | Lft -> "左"
+    | Rgt -> "右"
+
+  let stringizePlace (plId, vx) =
+    sprintf "[%s%s]"
+      (stringizeSide plId)
+      (stringizeVertex vx)
+
+  let printCardName g cardId =
+    let card    = g |> Game.card cardId
+    let name    = card |> Card.name
+    let st      = card |> Card.curStatus
+    in
+      match g |> Game.searchBoardFor cardId with
+      | Some place ->
+          printf "%s%s[%d/%d/%d]"
+            name
+            (stringizePlace place)
+            (st |> Status.hp)
+            (st |> Status.at)
+            (st |> Status.ag)
+      | None ->
+          printf "《%s》"  name
+
+  let printEvent (ev, g, g') =
+    match ev with
+    | GameBegin ->
+        printfn "-------- Game Begin --------"
+        printfn "%s vs %s"
+          (g |> Game.plLft |> Player.name)
+          (g |> Game.plRgt |> Player.name)
+
+    | GameEnd Draw ->
+        printfn "Draw game."
+
+    | GameEnd (Win plId) ->
+        printfn "%s win!"
+          (g' |> Game.player plId |> Player.name)
+
+    | TurnBegin ->
+        printfn "---- Turn %d ----"
+          (g' |> Game.turn)
+
+    | CardEnter (cardId, _) ->
+        printCardName g' cardId
+        printfn " is summoned!"
+
+    | CardActBegin (cardId, (name, _)) ->
+        printCardName g' cardId
+        printfn " does '%s'!"
+          name
+
+    | CardHpInc (cardId, amount) ->
+        let curHp = Game.card cardId >> Card.curHp
+        do
+          printCardName g' cardId
+          printfn "'s HP%d→%d (%+d)"
+            (g  |> curHp)
+            (g' |> curHp)
+            amount
+
+    | CardDie cardId ->
+        printCardName g cardId  // 死亡する直前の状態を表示
+        printfn " died."
+
+    | CardGainEffect (cardId, keff) ->
+        printCardName g' cardId
+        printfn " gained %A."  // TODO: 分かりやすく表示する
+          keff
+
+    | CardLoseEffect (cardId, keff) ->
+        printCardName g' cardId
+        printfn " lost %A."
+          keff
+
+    | CardMove moves ->
+        for (cardId, _, dst) in moves do
+          printCardName g cardId  // 移動前の位置を表示する
+          printfn " moves to %s."
+            (stringizePlace dst)
+
+  let subscribe g: IDisposable =
+    g
+    |> Game.asObservable
+    |> Observable.subscribe printEvent
--- TriTrain.Cui/Program.fs
+++ TriTrain.Cui/Program.fs
@@ -1,7 +1,54 @@
 namespace TriTrain.Cui
 
+open TriTrain.Core
+open TriTrain.Core.Serialize
+open Chessie.ErrorHandling
+
 module Program =
+  let loadDefaultPlayers =
+      trial {
+        let! deck1 = DeckSpec.load "deck1.yaml"
+        let! deck2 = DeckSpec.load "deck2.yaml"
+        let pl1 =
+          {
+            Name      = "左人"
+            Deck      = deck1
+          }
+        let pl2 =
+          {
+            Name      = "右人"
+            Deck      = deck2
+          }
+        return (pl1, pl2)
+      }
+
+  let runGameWithBroadcaster (pl1, pl2) =
+    let g = Game.create pl1 pl2
+    use o = Broadcaster.subscribe g
+    let _ = g |> Game.run
+    in ()
+
+  let showErrorMessages r =
+    let eprintAll = List.iter (eprintfn "%s")
+    match r with
+    | Pass _ -> ()
+    | Warn (_, msgs) ->
+        eprintfn "Warning:"
+        eprintAll msgs
+    | Fail msgs ->
+        eprintfn "Fatal error:"
+        eprintAll msgs
+
+  let testPlay () =
+    trial {
+      let! (pl1, pl2) = loadDefaultPlayers
+      do runGameWithBroadcaster (pl1, pl2)
+    }
+    |> showErrorMessages
+
   [<EntryPoint>]
-  let main argv = 
+  let main argv =
+    testPlay ()
+
     // exit code
     0
