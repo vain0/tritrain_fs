166ae1141bfdc85c68ece30c96bbcd5597c61b9d
vain0x <vainzerox@gmail.com>
2016-04-08 00:03:08 +0900

    Specify types of functions in `Util.fs`

--- TriTrain.Core/Util.fs
+++ TriTrain.Core/Util.fs
@@ -7,11 +7,11 @@ open Chessie.ErrorHandling
 
 [<AutoOpen>]
 module Misc =
-  let tap f x = f x; x
+  let tap (f: 'x -> unit) (x: 'x): 'x = f x; x
 
   let flip f x y = f y x
 
-  let isInInterval l r x =
+  let isInInterval l r x: bool =
     l <= x && x < r
 
   type T3<'t> = 't * 't * 't
@@ -21,10 +21,10 @@ module Misc =
 
 [<AutoOpen>]
 module T7 =
-  let toList (x0, x1, x2, x3, x4, x5, x6) =
+  let toList (x0, x1, x2, x3, x4, x5, x6): list<_> =
     [x0; x1; x2; x3; x4; x5; x6]
 
-  let ofList =
+  let ofList: list<'t> -> option<T7<'t>> =
     function
     | [x0; x1; x2; x3; x4; x5; x6] ->
         (x0, x1, x2, x3, x4, x5, x6) |> Some
@@ -33,19 +33,19 @@ module T7 =
   let init (f: int -> 't): T7<'t> =
     (f 0, f 1, f 2, f 3, f 4, f 5, f 6)
 
-  let map f (x0, x1, x2, x3, x4, x5, x6) =
+  let map (f: 'x -> 'y) ((x0, x1, x2, x3, x4, x5, x6): T7<'x>): T7<'y> =
     (f x0, f x1, f x2, f x3, f x4, f x5, f x6)
 
 [<RequireQualifiedAccess>]
 module Seq =
-  let product xs ys =
+  let product (xs: seq<'x>) (ys: seq<'y>): seq<'x * 'y> =
     seq {
       for x in xs do
       for y in ys -> (x, y) }
 
 [<RequireQualifiedAccess>]
 module Option =
-  let getOr x =
+  let getOr (x: 'x): option<'x> -> 'x =
     function
     | Some x -> x
     | None -> x
@@ -54,13 +54,13 @@ module Option =
 module List =
   /// List.zip を行う。
   /// 長さが異なる場合は、短いほうに合わせて縮める。
-  let zipShrink l r =
+  let zipShrink (l: list<'l>) (r: list<'r>): list<'l * 'r> =
     let len = min (l |> List.length) (r |> List.length)
     let l = l |> List.take len
     let r = r |> List.take len
     in List.zip l r
 
-  let tryMaxBy proj xs =
+  let tryMaxBy (proj: 'x -> 'y) (xs: list<'x>): option<'x> =
     xs
     |> List.fold (fun ma x ->
         let projX = proj x
@@ -86,10 +86,10 @@ module List =
 
 [<RequireQualifiedAccess>]
 module Map =
-  let singleton k v =
+  let singleton (k: 'k) (v: 'v): Map<'k, 'v> =
     Map.ofList [(k, v)]
 
-  let append l r =
+  let append (l: Map<'k, 'v>) (r: Map<'k, 'v>): Map<'k, 'v> =
     r |> Map.fold (fun l k v -> l |> Map.add k v) l
 
   let keySet (m: Map<'k, 'v>): Set<'k> =
@@ -101,7 +101,7 @@ module Map =
   let valueSet (m: Map<'k, 'v>): Set<'v> =
     m |> valueList |> Set.ofList
 
-  let pullBack value m =
+  let pullBack (value: 'v) (m: Map<'k, 'v>): Set<'k> =
     m
     |> Map.toList
     |> List.choose (fun (k, v) ->
@@ -109,7 +109,7 @@ module Map =
         )
     |> Set.ofList
 
-  let choose f m =
+  let choose (f: 'k -> 'v -> option<'w>) (m: Map<'k, 'v>): Map<'k, 'w> =
     m |> Map.fold (fun m k v ->
         match f k v with
         | None      -> m
@@ -124,11 +124,11 @@ module Reflection =
       FSharpType.GetUnionCases(typeof<'t>)
       |> Array.toList
 
-    static member val Names =
+    static member val Names: list<string> =
       DU<'t>.CaseInfos
       |> List.map (fun (case: UnionCaseInfo) -> case.Name)
 
-    static member TryParse(str) =
+    static member TryParse(str: string): option<'t> =
       DU<'t>.CaseInfos
       |> List.tryFind (fun case -> case.Name = str)
       |> Option.map (fun case -> FSharpValue.MakeUnion (case, [||]) :?> 't)
@@ -139,10 +139,10 @@ module Reflection =
             yield (FSharpValue.MakeUnion(ci, Array.empty) :?> 't, ci.Name)
       ] |> Map.ofList
 
-    static member StringizeUnitCase(case) =
+    static member StringizeUnitCase(case: 't): string =
       DU<'t>.StringizeUnitCaseMap |> Map.find case
 
-    static member val UnitCases =
+    static member val UnitCases: list<'t> =
       DU<'t>.StringizeUnitCaseMap |> Map.toList |> List.map fst
 
 [<RequireQualifiedAccess>]
@@ -153,7 +153,7 @@ module Random =
     rng.NextDouble() < prob
     || prob >= 100.0
 
-  let element xs =
+  let element (xs: seq<'x>): option<'x> =
     let len = xs |> Seq.length
     in
       if len = 0
@@ -164,14 +164,14 @@ module Random =
 module Observable =
   open System.Diagnostics
 
-  let indexed obs =
+  let indexed (obs: IObservable<'x>): IObservable<'x * int> =
     obs
     |> Observable.scan
         (fun (opt, i) x -> (Some x, i + 1)) (None, -1)
     |> Observable.choose
         (fun (opt, i) -> opt |> Option.map (fun x -> (x, i)))
 
-  let duplicateFirst obs =
+  let duplicateFirst (obs: IObservable<'x>): IObservable<'x> =
     let obs' =
       obs
       |> indexed
@@ -244,7 +244,7 @@ module Observable =
       finished <- true
       error err
 
-    member this.AsObservable = obs
+    member this.AsObservable: IObservable<'t> = obs
 
 type BatchedQueue<'T> =
   | BatchedQueue of forwardList: list<'T> * reversedList: list<'T>
@@ -262,35 +262,35 @@ module BatchedQueue =
       | ([], r) -> BatchedQueue (r |> List.rev, [])
       | _ -> self
 
-  let empty =
+  let empty: BatchedQueue<_> =
     BatchedQueue ([], [])
 
-  let add x self =
+  let add (x: 'x) (self: BatchedQueue<'x>): BatchedQueue<'x> =
     self
     |> unwrap
     |> function (l, r) -> BatchedQueue (l, x :: r)
     |> rebuild
 
-  let tryUncons self =
+  let tryUncons (self: BatchedQueue<'x>): option<'x * BatchedQueue<'x>> =
     self
     |> unwrap
     |> function
       | ([], _) -> None
       | (x :: l, r) -> Some (x, BatchedQueue (l, r) |> rebuild)
 
-  let tryHead self =
+  let tryHead (self: BatchedQueue<'x>): option<'x> =
     self |> tryUncons |> Option.map fst
 
-  let tryTail self =
+  let tryTail (self: BatchedQueue<'x>): option<BatchedQueue<'x>> =
     self |> tryUncons |> Option.map snd
 
-  let toList self =
+  let toList (self: BatchedQueue<'x>): list<'x> =
     self
     |> unwrap
     |> function (f, r) -> List.append f (List.rev r)
 
-  let ofList self =
-    BatchedQueue (self, [])
+  let ofList (xs: list<'x>): BatchedQueue<'x> =
+    BatchedQueue (xs, [])
 
   let toSeq self = self |> toList |> List.toSeq
   let ofSeq self = self |> Seq.toList |> ofList
@@ -299,23 +299,23 @@ module BatchedQueue =
     ofList [x]
 
   // Note: キューが空でないときは常に、前方リストが空でない。
-  let isEmpty (BatchedQueue (l, _)) =
+  let isEmpty (BatchedQueue (l, _)): bool =
     List.isEmpty l
 
-  let length self =
+  let length (self: BatchedQueue<_>): int =
     self
     |> unwrap
     |> function (l, r) -> List.length l + List.length r
 
-  let map f self =
+  let map (f: 'x -> 'y) (self: BatchedQueue<'x>): BatchedQueue<'y> =
     self
     |> unwrap
     |> function (l, r) -> BatchedQueue (List.map f l, List.map f r)
 
-  let fold f s self =
+  let fold (f: 's -> 'x -> 's) (s: 's) (self: BatchedQueue<'x>): 's =
     self |> toList |> List.fold f s
 
-  let append l r =
+  let append (l: BatchedQueue<'x>) (r: BatchedQueue<'x>): BatchedQueue<'x> =
     r |> fold (fun self x -> self |> add x) l
 
 [<AutoOpen>]
@@ -332,7 +332,7 @@ module TrialOperators =
 module Yaml =
   open FsYaml
 
-  let myTryLoad<'t> yaml =
+  let myTryLoad<'t> (yaml: string): Result<'t, exn> =
     try
       Yaml.load<'t> yaml
       |> pass
