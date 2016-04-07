7c9c92286bbb65f6458fd1870b86c6bba998b0cf
vain0x <vainzerox@gmail.com>
2016-04-07 23:53:19 +0900

    [cui] Add `rand` command

---
+++ TriTrain.Cui/Random.fs
@@ -0,0 +1,18 @@
+﻿namespace TriTrain.Cui
+
+open TriTrain.Core
+open TriTrain.Core.Serialize
+open Chessie.ErrorHandling
+
+module Random =
+  let (|RandomCommand|_|): list<string> -> option<Result<unit, string>> =
+    function
+    | "rand" :: _ ->
+        trial {
+          return
+            Random.deckSpec ()
+            |> DeckSpec.toSrc 
+            |> DeckSpecSrc.serialize
+            |> printfn "%s"
+        } |> Some
+    | _ -> None
