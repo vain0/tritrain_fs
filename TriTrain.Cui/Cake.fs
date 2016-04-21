f9e515f0c0f1059b8a31121466ab18b33062cdb6
vain0x <vainzerox@gmail.com>
2016-04-21 22:06:18 +0900

    [cui/cake] Add `requestJsonAsync`

--- TriTrain.Cui/Cake.fs
+++ TriTrain.Cui/Cake.fs
@@ -2,10 +2,21 @@
 
 open TriTrain.Core
 open System
+open System.Net
 open Chessie.ErrorHandling
+open FSharp.Data
 
 module Cake =
   let url = @"http://vain0.s2.xrea.com/tritrain_cake/services"
+
+  let requestJsonAsync args =
+    async {
+      let url = url + "/" + (args |> String.concat "/")
+      let req = WebRequest.CreateHttp(url)
+      req.Method <- WebRequestMethods.Http.Get
+      req.Accept <- "application/json"
+      return! req.GetResponseAsync() |> Async.AwaitTask
+    }
 
   let (|Command|_|) =
     function
