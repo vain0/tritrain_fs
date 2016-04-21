ec84922a72fb26c8c179374d43cfa79c2416aeb4
vain0x <vainzerox@gmail.com>
2016-04-21 22:53:08 +0900

    [cui/cake] Remake `url` as a function

--- TriTrain.Cui/Cake.fs
+++ TriTrain.Cui/Cake.fs
@@ -7,12 +7,13 @@ open System.IO
 open Chessie.ErrorHandling
 
 module Cake =
-  let url = @"http://vain0.s2.xrea.com/tritrain_cake/services"
+  let url args =
+    @"http://vain0.s2.xrea.com/tritrain_cake/services/"
+    + (args |> String.concat "/")
 
   let requestJsonAsync args =
     async {
-      let url = url + "/" + (args |> String.concat "/")
-      let req = WebRequest.CreateHttp(url)
+      let req = WebRequest.CreateHttp(url args)
       req.Method <- WebRequestMethods.Http.Get
       req.Accept <- "application/json"
       return! req.GetResponseAsync() |> Async.AwaitTask
